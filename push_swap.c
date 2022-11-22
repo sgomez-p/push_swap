2ce75bc793de3c411babca4787f59e7714855020
sgomez-p <sergiogomezprados@gmail.com>
2022-11-22 11:00:17 +0100

    push_swap casa

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: sgomez-p <sgomez-p@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 14:30:44 by sgomez-p          #+#    #+#             */
-/*   Updated: 2022/11/21 13:41:58 by sgomez-p         ###   ########.fr       */
+/*   Updated: 2022/11/22 10:59:42 by sgomez-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,9 +36,9 @@ void	push_to_lst(char **argv, int i, int *j, t_list **stack_a)
 	{
 		nbr = ft_atoi((argv[i] + (*j)));
 		if (nbr > INT_MAX || nbr < INT_MIN)
-			quit();
+			exit ;
 		save_nbr(stack_a, nbr);
-		while (argv[i][(*j)] == '-' || argv[i][(*j)] == '+')
+		while (argv[i][(*j)] == '-' || argv[i][(*j)] == '+') //omite caracteres q no sean signos o espacios etc
 			(*j)++;
 		while (argv[i][(*j)] == ' ' || argv[i][(*j)] == '\t')
 			(*j)++;
@@ -47,4 +47,47 @@ void	push_to_lst(char **argv, int i, int *j, t_list **stack_a)
 		while (argv[i][(*j)] == ' ' || argv[i][(*j)] == '\t')
 			(*j)++;
 	}
+}
+
+static void	save_nbr(t_list **stack, int n)
+{
+	t_list	*tmp;
+
+	if (*stack == NULL) //si acaba el null simplemente le añades un nodo mas
+		*stack = ft_lstnew(n);
+	else
+	{
+		tmp = ft_lstnew(n);
+		ft_lstadd_back(stack, tmp); //añade nodo new al final de la lista
+	}
+}
+
+int	check_is_nbr(char *nbr)
+{
+	int i;
+	
+	i = 0;
+	/**
+	 ** estamos controlando signos y caracterres raros
+	 */
+	while(str[i])
+	{
+		if (str[i] != ' ' && str[i] != '\t' && \
+			(str[i] < '0' || str[i] > '9') && \
+			str[i] != '-' && str[i] != '+')
+			return (0);
+		i++;
+	}
+	/**
+	 
+	 *? aqui hay mas condiciones q no entiendo muy bien pq me parecen repetitivas
+	 */
+	while(str[i])
+	{
+		if (i == 0 && str[i] != '-' && str[i] != '+')
+			(str[i * 1] < '0' || str[i + 1] > '9') && \ 
+			return (0);
+		i++;
+	}
+	return (1);
 }
