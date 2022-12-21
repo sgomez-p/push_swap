e3bee3f242d88411a02750361480e337cc0efd76
sgomez-p <sergiogomezprados@gmail.com>
2022-12-21 10:10:07 +0100

    pilla numeros y hace el stack

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: sgomez-p <sgomez-p@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 14:30:44 by sgomez-p          #+#    #+#             */
-/*   Updated: 2022/12/19 15:51:41 by sgomez-p         ###   ########.fr       */
+/*   Updated: 2022/12/20 19:45:19 by sgomez-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,34 @@
 
 void	create_stack(t_stacks *situ, int argc, char **argv)
 {
-	int				i;
-	t_stack			*current;
+	t_stack *current;
+	int i;
 
-	(current = (t_stack *)malloc(sizeof(t_stack)));
+	current = (t_stack *)malloc(sizeof(t_stack));
 	if (!current)
-		exit(1);
+	{
+		fprintf(stderr, "Error al reservar memoria para el stack_a.\n");
+		exit(EXIT_FAILURE);
+	}
 	situ->stack_a = current;
-	i = 0;
-	if (!(current->next))
-		exit(1);
-	while (++i < argc)
+	current->next = NULL;
+	i = 1;
+	while (i < argc)
 	{
 		current->n = ft_atoi(argv[i]);
-		current->next = NULL;
 		if (i + 1 < argc)
 		{
 			current->next = (t_stack *)malloc(sizeof(t_stack));
 			if (!(current->next))
-				exit(1);
-		}
+			{
+				fprintf(stderr, "Error al reservar memoria para el stack_a.\n");
+				exit(EXIT_FAILURE);
+			}
 		current = current->next;
-		if (i == (argc - 1))
-			current->next = NULL;
-		else
-			current = current->next;
+		current->next = NULL;
+		}
 		i++;
 	}
-	free (current);
 }
 
 void	write_stack_a(t_stack *stack_a)
@@ -51,33 +51,46 @@ void	write_stack_a(t_stack *stack_a)
 	current = stack_a;
 	while (current != NULL)
 	{
-		ft_putnbr((int)current->n);
-		while (current->next)
-			current = current->next;
+		ft_putnbr(current->n);
+		ft_putchar('\n');
+		current = current->next;
 	}
-	ft_putstr("\n");
-	free(current);
 }
 
 void	write_stack_b(t_stack *stack_b)
 {
 	t_stack	*current;
 
+	if (stack_b == NULL)
+	{
+		ft_putstr("         (vacío)");
+		return;
+	}
 	current = stack_b;
 	while (current != NULL)
 	{
 		ft_putstr("         ");
 		ft_putnbr(current->n);
-		while (current->next)
-			current = current->next;
+		current = current->next;
 	}
 }
 
 int	main(int argc, char **argv)
 {
-	t_stacks	current;
-
+	t_stacks current;
+	if (argc < 2)
+	{
+		fprintf(stderr, " %s Introduce números a ordenar\n", argv[0]);
+		return EXIT_FAILURE;
+	}
 	create_stack(&current, argc, argv);
 	write_stack_a(current.stack_a);
+	ft_putstr("-------\n");
+	ft_putstr("a");
+	ft_putstr("    ");
 	write_stack_b(current.stack_b);
+	ft_putstr("b");
+
+	//return EXIT_SUCCESS;
+
 }
